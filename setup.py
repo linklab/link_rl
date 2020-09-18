2212fa0adb70a4ce7b5b55de5e420165267b6cd4
Youn-Hee Han <yh21.han@gmail.com>
2020-09-18 17:46:00 +0900

    ....

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import io
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 # Read in the README for the long description on PyPI
@@ -16,7 +16,7 @@ setup(name='link_rl',
       author='link.koreatech',
       author_email='link.koreatech@gmail.com',
       license='MIT',
-      packages=find_packages(),
+      packages='link_rl',
       classifiers=[
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
