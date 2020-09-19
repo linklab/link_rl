73106d475fb6b169be6fd275e3c4101d3c4b906a
Youn-Hee Han <yh21.han@gmail.com>
2020-09-19 21:05:32 +0900

    ...

--- lib/fast_rl/common/wrappers.py
+++ lib/fast_rl/common/wrappers.py
@@ -7,6 +7,7 @@ from gym import spaces
 from skimage.transform import resize
 from skimage.color import rgb2gray
 
+
 class NoopResetEnv(gym.Wrapper):
     def __init__(self, env=None, noop_max=30):
         """Sample initial states by taking random number of no-ops on reset.
