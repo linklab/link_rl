2795a294746b8e071faa68ca74cb02ce2c803086
Youn-Hee Han <yh21.han@gmail.com>
2020-09-19 21:09:27 +0900

    ....

--- setup.py
+++ setup.py
@@ -9,18 +9,22 @@ def long_description():
     return readme
 
 
-setup(name='link_rl',
-      version='0.1',
-      description='reinforcement learning library and executables by LINK@KOREATECH',
-      long_description=long_description(),
-      url='https://github.com/linklab/link_rl.git',
-      author='link.koreatech',
-      author_email='link.koreatech@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      classifiers=[
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-      ],
-      zip_safe=False
+setup(
+    name='link_rl',
+    version='0.1',
+    description='reinforcement learning library and executables by LINK@KOREATECH',
+    long_description=long_description(),
+    url='https://github.com/linklab/link_rl.git',
+    author='link.koreatech',
+    author_email='link.koreatech@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=[
+        'visdom==0.1.8.9'
+    ],
+    zip_safe=False
 )
