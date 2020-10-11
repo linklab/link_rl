e6862c0ff1be423b6ae998440c2701ec2523ea32
Youn-Hee Han <yh21.han@gmail.com>
2020-10-11 16:33:45 +0900

    ...

--- rl_main/base/06_pendulum_ddpg_main.py
+++ rl_main/base/06_pendulum_ddpg_main.py
@@ -189,6 +189,7 @@ def main():
                     loss_actor, loss_critic, loss_total, len(buffer.buffer)
                 )
 
+
 def model_update(buffer, actor_net, critic_net, target_actor_net, target_critic_net, actor_optimizer, critic_optimizer, stat_for_ddpg, step_idx, exp,
                  actor_grad_l2, actor_grad_max, actor_grad_variance,
                  critic_grad_l2, critic_grad_max, critic_grad_variance,
---
+++ rl_main/base/06_pendulum_ddpg_play.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import gym
+import torch
+import os
+
+MODEL_SAVE_DIR = os.path.join(".", "saved_models")
+
+print(torch.__version__)
+
+from common.fast_rl import actions, value_based_model, rl_agent, policy_based_model
+import numpy as np
+
+from config.parameters import PARAMETERS as params
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+if torch.cuda.is_available():
+    device = torch.device("cuda" if params.CUDA else "cpu")
+else:
+    device = torch.device("cpu")
+
+
+def play_main():
+    env = gym.make(params.ENVIRONMENT_ID.value)
+
+    actor_net = policy_based_model.DDPGActor(
+        obs_size=3,
+        hidden_size_1=512, hidden_size_2=256,
+        n_actions=1
+    ).to(device)
+    print(actor_net)
+
+    rl_agent.load_model(MODEL_SAVE_DIR, params.ENVIRONMENT_ID.value, actor_net.__name__, net)
+
+    action_selector = actions.ArgmaxActionSelector()
+    agent = rl_agent.DQNAgent(net, action_selector, device=device)
+
+    done = False
+    state = env.reset()
+
+    while not done:
+        env.render()
+        state = np.expand_dims(state, axis=0)
+        action = agent(state)
+        next_state, reward, done, info = env.step(action[0][0])
+        state = next_state
+
+
+if __name__ == "__main__":
+    play_main()
