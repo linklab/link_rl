90084e2c5b84f8a0a99ce7dbb1d4668e59c804af
Youn-Hee Han <yh21.han@gmail.com>
2020-10-23 07:45:18 +0900

    ...

--- common/environments/matlab/matlabenv.py
+++ common/environments/matlab/matlabenv.py
@@ -56,7 +56,7 @@ class MatlabRotaryInvertedPendulumEnv(gym.Env):
         ]
 
         print(self.q)
-        
+
         self.state = (math.cos(self.q), math.sin(self.q), self.w)
         reward = -((math.pi - self.q) ** 2 + 0.1 * (self.w ** 2) + 0.001 * (action ** 2))
         info = [None]
