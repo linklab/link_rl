7d744e8fdddbd129a97dfca432931fe2d6d25add
DESKTOP-U5MLG6R\wlckd <wlckd5660@gmail.com>
2020-10-22 17:09:05 +0900

    ...

--- common/environments/matlab/matlabenv.py
+++ common/environments/matlab/matlabenv.py
@@ -75,7 +75,6 @@ class MatlabRotaryInvertedPendulumEnv(gym.Env):
 
 
         self.state, reward, info = (math.cos(self.q), math.sin(self.q), self.w), (-self.q)**2+(0.1*(self.w**2))+(0.001*(action**2)), [None]
-        print(self.state)
         # self.state, reward, info = (self.q/math.pi, self.q1, self.w, self.w1),(self.q)**2+(0.1*(self.w**2))+(0.001*(action**2)), [None]
         self.next_obs_degree[0] = self.convert_radian_to_degree(np.round(self.state, decimals=4)[0] * math.pi)
         self.next_obs_degree[1] = self.convert_radian_to_degree(np.round(self.state, decimals=4)[1])
