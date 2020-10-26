479768292b2c2b6efd1cc8fd3339a0354d632d01
DESKTOP-U5MLG6R\wlckd <wlckd5660@gmail.com>
2020-10-26 20:31:19 +0900

    ...

--- rl_main/matlab_pendulum_main/MATLAB_pendulum_ddpg_main.py
+++ rl_main/matlab_pendulum_main/MATLAB_pendulum_ddpg_main.py
@@ -38,10 +38,8 @@ if torch.cuda.is_available():
     device = torch.device("cuda" if params.CUDA else "cpu")
 else:
     device = torch.device("cpu")
-if params.CH:
-    SCALE_FACTOR = 0.01
-else:
-    SCALE_FACTOR = 0.025
+
+SCALE_FACTOR = 0.025
 
 def play_func(exp_queue, env, actor_net, critic_net):
     # print(env.action_space.low[0], env.action_space.high[0])
