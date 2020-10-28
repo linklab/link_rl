dd3ae83b5955b3e0ea7893c3509330d174a1f59c
Youn-Hee Han <yh21.han@gmail.com>
2020-10-28 23:12:40 +0900

    PER OFF

--- rl_main/matlab_pendulum_main/MATLAB_pendulum_ddpg_main.py
+++ rl_main/matlab_pendulum_main/MATLAB_pendulum_ddpg_main.py
@@ -152,10 +152,10 @@ def main():
     actor_optimizer = optim.Adam(actor_net.parameters(), lr=params.ACTOR_LEARNING_RATE)
     critic_optimizer = optim.Adam(critic_net.parameters(), lr=params.LEARNING_RATE)
 
-    # buffer = experience.ExperienceReplayBuffer(experience_source=None, buffer_size=params.REPLAY_BUFFER_SIZE)
-    buffer = experience.PrioritizedReplayBuffer(
-        experience_source=None, buffer_size=params.REPLAY_BUFFER_SIZE, n_step=params.N_STEP
-    )
+    buffer = experience.ExperienceReplayBuffer(experience_source=None, buffer_size=params.REPLAY_BUFFER_SIZE)
+    # buffer = experience.PrioritizedReplayBuffer(
+    #     experience_source=None, buffer_size=params.REPLAY_BUFFER_SIZE, n_step=params.N_STEP
+    # )
 
     exp_queue = mp.Queue(maxsize=params.TRAIN_STEP_FREQ * 2)
     play_proc = mp.Process(target=play_func, args=(exp_queue, env, actor_net, critic_net))
@@ -238,7 +238,7 @@ def main():
                     buffer, actor_net, critic_net, target_actor_net, target_critic_net, actor_optimizer, critic_optimizer,
                     step_idx, actor_grad_l2, actor_grad_max, actor_grad_variance,
                     critic_grad_l2, critic_grad_max, critic_grad_variance,
-                    loss_actor, loss_critic, loss_total, per=True
+                    loss_actor, loss_critic, loss_total, per=False
                 )
 
 
