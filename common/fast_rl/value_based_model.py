4c0f92eb8cfe2b346adf0655e601ddd9ac6e5a1e
hwang <to6289@gmail.com>
2020-10-29 13:56:09 +0900

    ...

--- common/fast_rl/value_based_model.py
+++ common/fast_rl/value_based_model.py
@@ -593,7 +593,7 @@ def calc_loss_per_double_dqn(buffer, batch, batch_indices, batch_weights, net, t
 
         expected_state_action_values = next_state_values.detach() * (params.GAMMA ** last_steps_v) + rewards_v
 
-    losses_each = F.smooth_l1_loss(state_action_values, expected_state_action_values, reduction='none')
+    losses_each = F.smooth_l1_loss(state_action_values, expected_state_action_values.detach(), reduction='none')
     weighted_losses_v = batch_weights_v.detach() * losses_each
 
     return weighted_losses_v.mean(), losses_each + 1e-5
@@ -628,7 +628,7 @@ def calc_loss_per_double_dqn_for_omega(buffer, batch, batch_indices, batch_weigh
     if cuda:
         expected_state_action_values = expected_state_action_values.cuda(non_blocking=cuda_async)
 
-    losses_each = F.smooth_l1_loss(state_action_values, expected_state_action_values, reduction='none')
+    losses_each = F.smooth_l1_loss(state_action_values, expected_state_action_values.detach(), reduction='none')
     weighted_losses_v = batch_weights_v.detach() * losses_each
 
     return weighted_losses_v.mean(), losses_each + 1e-5
