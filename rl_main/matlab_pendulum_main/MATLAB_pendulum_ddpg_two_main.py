10be3cfaf845b5316f7bf79d0d7675cd10685151
Youn-Hee Han <yh21.han@gmail.com>
2020-11-28 23:43:29 +0900

    ...

--- rl_main/matlab_pendulum_main/MATLAB_pendulum_ddpg_two_main.py
+++ rl_main/matlab_pendulum_main/MATLAB_pendulum_ddpg_two_main.py
@@ -184,10 +184,13 @@ def play_func(exp_queue_swing_up, exp_queue_balancing, actor_swing_up_net, criti
 
                 exp_queue_balancing.put(exp)
 
+                balancing_step_reward_list.append(exp.reward)
+
                 # NOTE: 대기 중인 exp의 reward를 수정하고 exp_queue_swing_up에 넣기
                 recent_swing_up_to_balancing_exp._replace(reward=sum(balancing_step_reward_list))
                 exp_queue_swing_up.put(recent_swing_up_to_balancing_exp)
 
+                recent_swing_up_to_balancing_exp = None
                 balancing_step_reward_list.clear()
 
             else:
