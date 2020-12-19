272f43d3b1bed7762e83b2a4aab492b84d817baf
Youn-Hee Han <yh21.han@gmail.com>
2020-12-19 09:59:24 +0900

    DOUBLE 일 때 DQN Action 개수 17 --> 21

--- common/environments/matlab/matlabenv.py
+++ common/environments/matlab/matlabenv.py
@@ -51,8 +51,8 @@ class MatlabRotaryInvertedPendulumEnv(gym.Env):
             ]
         elif self.pendulum_type == 'PENDULUM_MATLAB_DOUBLE_RIP_V0':
             self.action_index_to_voltage = [
-                -1.0, -0.75, -0.35, -0.10, -0.05, -0.025, -0.016, -0.004, 0.0,
-                0.004, 0.016, 0.025, 0.05, 0.10, 0.35, 0.75, 1.0
+                -3.0, -2.5, -2.0, -1.5, -0.75, -0.35, -0.10, -0.05, -0.025, -0.016, 0.0,
+                0.016, 0.025, 0.05, 0.10, 0.35, 0.75, 1.5, 2.0, 2.5, 3.0
             ]
         else:
             raise ValueError()
