a0f088ea9e5a32a27feba03fc8056a8aeedef622
Youn-Hee Han <yh21.han@gmail.com>
2021-01-12 20:09:50 +0900

    grpc

---
+++ temp/grpc/server/grpc_server.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from concurrent import futures
+
+import grpc
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+PROJECT_HOME = os.path.abspath(os.path.join(current_path, os.pardir, os.pardir, os.pardir))
+if PROJECT_HOME not in sys.path:
+    sys.path.append(PROJECT_HOME)
+
+from temp.grpc import helloworld_pb2_grpc, helloworld_pb2
+
+
+class Greeter(helloworld_pb2_grpc.GreeterServicer):
+    def SayHello(self, request, context):
+        return helloworld_pb2.HelloReply(message='Hello, {0}!'.format(request.name))
+
+    def SayHelloAgain(self, request, context):
+        return helloworld_pb2.HelloReply(message='Hello again, {0}!'.format(request.name))
+
+
+def serve_main():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve_main()
