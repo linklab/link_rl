29fb561657a968b8754817a75c44fcce9abfeeb3
JJBong <jubong1992@gmail.com>
2021-02-17 16:12:42 +0900

    ...

--- codes/e_utils/rl_utils.py
+++ codes/e_utils/rl_utils.py
@@ -316,19 +316,3 @@ def get_optimizer(parameters, learning_rate, params):
         optimizer = None
 
     return optimizer
-
-
-class Scheduler(object):
-    def __init__(self, v, nvalues, schedule):
-        self.n = 0.
-        self.v = v
-        self.nvalues = nvalues
-        self.schedule = schedules[schedule]
-
-    def value(self):
-        current_value = self.v*self.schedule(self.n/self.nvalues)
-        self.n += 1.
-        return current_value
-
-    def value_steps(self, steps):
-        return self.v*self.schedule(steps/self.nvalues)
