5b8f3dec48f1963e20d3391d03f454e55eb99894
Youn-Hee Han <yh21.han@gmail.com>
2021-02-21 23:16:21 +0900

    ...

--- codes/f_main/general_main/main.py
+++ codes/f_main/general_main/main.py
@@ -153,7 +153,7 @@ def play_func(exp_queue, agent):
                         if episode % params.EARLY_STOPPING_TEST_EPISODE_PERIOD == 0:
                             if params.MODEL_SAVE_MODE in [ModelSaveMode.TRAIN, ModelSaveMode.TEST]:
                                 if params.MODEL_SAVE_MODE == ModelSaveMode.TRAIN:
-                                    test_mean_episode_reward = np.mean(train_episode_reward_lst_for_test)
+                                    test_mean_episode_reward = np.mean(train_episode_reward_lst_for_test).item()
                                     test_std = np.std(train_episode_reward_lst_for_test)
                                     train_episode_reward_lst_for_test.clear()
                                     test_env_str = colored("TRAIN ENV", "yellow")
