25d6bf285b620fbc7e37e29811bbffee2b66478c
Youn-Hee Han <yh21.han@gmail.com>
2021-05-09 22:12:38 +0900

    ANT_TD3

--- codes/a_config/c_pybullet_parameters/parameters_ant_td3.py
+++ codes/a_config/c_pybullet_parameters/parameters_ant_td3.py
@@ -1,30 +1,32 @@
-from codes.a_config._rl_parameters.off_policy.parameter_ddpg import PARAMETERS_DDPG
+from codes.a_config._rl_parameters.off_policy.parameter_td3 import TD3ActionType, TD3ActionSelectorType, PARAMETERS_TD3
 from codes.e_utils.names import *
 from codes.a_config.parameters_general import PARAMETERS_GENERAL
 
 # https://github.com/bulletphysics/bullet3/blob/master/examples/pybullet/gym/pybullet_envs/minitaur/envs/minitaur_gym_env.py
-class PARAMETERS_ANT_DDPG(PARAMETERS_GENERAL, PARAMETERS_DDPG):
+class PARAMETERS_ANT_TD3(PARAMETERS_GENERAL, PARAMETERS_TD3):
     ENVIRONMENT_ID = EnvironmentName.PYBULLET_ANT_V0
-    DEEP_LEARNING_MODEL = DeepLearningModelName.DETERMINISTIC_CONTINUOUS_ACTOR_CRITIC_MLP
-    RL_ALGORITHM = RLAlgorithmName.DDPG_V0
+    DEEP_LEARNING_MODEL = DeepLearningModelName.TD3_MLP
+    RL_ALGORITHM = RLAlgorithmName.TD3_V0
     OPTIMIZER = OptimizerName.ADAM
 
     TRAIN_STOP_EPISODE_REWARD = 1500.0
     TRAIN_STOP_EPISODE_REWARD_STD = 50.0
     STOP_PATIENCE_COUNT = 10
     REPLAY_BUFFER_SIZE = 1000000
-    TARGET_NET_SYNC_STEP_PERIOD = 10000
     MAX_GLOBAL_STEP = 10000000
+
+    LEARNING_RATE = 0.00025
+    GAMMA = 0.99
+    BATCH_SIZE = 128
+    TRAIN_STEP_FREQ = 1
+    AVG_EPISODE_SIZE_FOR_STAT = 50
+    N_STEP = 1
 
     EPSILON_INIT = 1.0
     EPSILON_MIN = 0.01
     EPSILON_MIN_STEP = 3000000
 
-    LEARNING_RATE = 0.00025
-    GAMMA = 0.99
-    BATCH_SIZE = 32
-    TRAIN_STEP_FREQ = 1
-    AVG_EPISODE_SIZE_FOR_STAT = 50
-    N_STEP = 1
-
     ACTION_SCALE = 1.0
+
+    TYPE_OF_TD3_ACTION = TD3ActionType.GAUSSIAN_NOISE_WITH_EPSILON
+    TYPE_OF_TD3_ACTION_SELECTOR = TD3ActionSelectorType.BASIC_ACTION_SELECTOR
--- codes/a_config/parameters_preamble.py
+++ codes/a_config/parameters_preamble.py
@@ -40,6 +40,7 @@ from codes.a_config.c_pybullet_parameters.parameters_inverted_double_pendulum_bu
 from codes.a_config.c_pybullet_parameters.parameters_ant_ppo import PARAMETERS_ANT_PPO
 from codes.a_config.c_pybullet_parameters.parameters_ant_ddpg import PARAMETERS_ANT_DDPG
 from codes.a_config.c_pybullet_parameters.parameters_ant_a2c import PARAMETERS_ANT_A2C
+from codes.a_config.c_pybullet_parameters.parameters_ant_td3 import PARAMETERS_ANT_TD3
 
 from codes.a_config.d_matlab_parameters.parameters_matlab_pendulum_ppo import \
     PARAMETERS_PENDULUM_MATLAB_PPO
