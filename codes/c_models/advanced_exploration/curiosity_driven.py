9794c5aa193fc37232f1d19e789f1361709d927e
Youn-Hee Han <yh21.han@gmail.com>
2021-05-23 01:31:56 +0900

    ...

--- codes/c_models/advanced_exploration/curiosity_driven.py
+++ codes/c_models/advanced_exploration/curiosity_driven.py
@@ -56,6 +56,8 @@ class CuriosityCnnStateEncoder(nn.Module): #A
         return int(np.prod(o.size()))
 
     def forward(self, x):
+        if not (type(x) is torch.Tensor):
+            x = torch.tensor([x], dtype=torch.float).to(self.device)
         x = F.normalize(x)
         y = self.encoder(x)
         y = y.flatten(start_dim=1) #size N, 288
