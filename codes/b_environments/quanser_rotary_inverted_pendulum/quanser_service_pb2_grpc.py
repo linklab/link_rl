f94023fb1330b39c5b8aea1b994e309e44df09b3
DESKTOP-Q88LR15\wlckd <wlckd5660@gmail.com>
2021-06-01 17:59:57 +0900

    ...

--- codes/b_environments/quanser_rotary_inverted_pendulum/quanser_service_pb2_grpc.py
+++ codes/b_environments/quanser_rotary_inverted_pendulum/quanser_service_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import quanser_service_pb2 as quanser__service__pb2
+from . import quanser_service_pb2 as quanser__service__pb2
 
 
 class QuanserRIPStub(object):
