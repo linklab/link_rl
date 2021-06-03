53352ccc9307a28a732824276d38f45191dc269b
Youn-Hee Han <yh21.han@gmail.com>
2021-06-03 17:38:30 +0900

    ...

--- codes/a_config/e_real_device/parameters_rip_sac.py
+++ codes/a_config/e_real_device/parameters_rip_sac.py
@@ -1,5 +1,4 @@
 from codes.a_config._rl_parameters.off_policy.parameter_sac import PARAMETERS_SAC, SACActionType, SACActionSelectorType
-from codes.a_config._rl_parameters.off_policy.parameter_td3 import PARAMETERS_TD3, TD3ActionType, TD3ActionSelectorType
 from codes.e_utils.names import *
 from codes.a_config.parameters_general import PARAMETERS_GENERAL, RIPEnvRewardType
 
