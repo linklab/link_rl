fba51ef416b95d4f23b7b74ff7837c99b67e81af
zzzanghun <wlckd5660@gmail.com>
2021-06-04 17:09:25 +0900

    ...

--- codes/b_environments/rotary_inverted_pendulum/rip.py
+++ codes/b_environments/rotary_inverted_pendulum/rip.py
@@ -39,6 +39,8 @@ if params.ENVIRONMENT_ID in [EnvironmentName.REAL_DEVICE_RIP, EnvironmentName.RE
         RIP_SERVER = '10.0.0.10'
     elif params.SERVER_IDX == 1:
         RIP_SERVER = '10.0.0.11'
+    elif params.SERVER_IDX == 2:
+        RIP_SERVER = '10.0.0.9'
 
 
 class DoneReason(enum.Enum):
@@ -575,7 +577,7 @@ class RotaryInvertedPendulumEnv(gym.Env):
             #     time.sleep(0.008)
             #     num += 1
 
-            rip_response = self.server_obj.step(RipRequest(value=action))
+            rip_response = self.server_obj.step(RipRequest(value=0))
             self.motor_position = math.radians(rip_response.arm_angle)
             self.motor_velocity = rip_response.arm_velocity
             self.pendulum_1_position = math.radians(rip_response.link_1_angle)
@@ -583,6 +585,10 @@ class RotaryInvertedPendulumEnv(gym.Env):
             self.pendulum_2_position = math.radians(rip_response.link_2_angle)
             self.pendulum_2_velocity = rip_response.link_2_velocity
             self.simulation_time = None
+            # print("motor vel :{0:5.3f}, pen1_Vel : {1:5.3f}, pen2_vel : {2:5.3f}, motor posi :{3:5.3f}, pen1 posi :{4:5.3f}, pen2 posi :{4:5.3f}".format(
+            #     self.motor_velocity, self.pendulum_1_velocity, self.pendulum_2_velocity, self.motor_velocity, self.pendulum_1_position, self.pendulum_2_position
+            # ))
+            # time.sleep(0.5)
             # if rip_response.message == "FORCE_TERMINATE":
             #     print("FORCE TERMINATE !!!!!!!!!!!!!!!!!!")
             #     exit(-1)
@@ -995,13 +1001,13 @@ class RotaryInvertedPendulumEnv(gym.Env):
             position_reward = position_reward / 2.0
 
         reward = position_reward + energy_penalty
-        # print(
-        #     "position_reward_1: {0:3.4f}".format(adjusted_pendulum_1_position),
-        #     "position_Reward_2 : {0:3.4f}".format(reward_pendulum_2),
-        #     "position_reward: {0:3.4f}".format(position_reward),
-        #     "energy_penalty: {0:3.4f}".format(energy_penalty),
-        #     "reward : {0:3.4f}".format(reward)
-        # )
+        print(
+            "position_reward_1: {0:3.4f}".format(adjusted_pendulum_1_position),
+            "position_Reward_2 : {0:3.4f}".format(reward_pendulum_2),
+            "position_reward: {0:3.4f}".format(position_reward),
+            "energy_penalty: {0:3.4f}".format(energy_penalty),
+            "reward : {0:3.4f}".format(reward)
+        )
 
         reward = max(0.0, reward)
         return reward
