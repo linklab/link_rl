86740a5a2832f59f42db7a6ce1d57e0a51bc629e
zzzanghun <wlckd5660@gmail.com>
2021-06-21 11:56:36 +0900

    ...

--- codes/b_environments/rotary_inverted_pendulum/rip.py
+++ codes/b_environments/rotary_inverted_pendulum/rip.py
@@ -601,7 +601,7 @@ class RotaryInvertedPendulumEnv(gym.Env):
             #     time.sleep(0.008)
             #     num += 1
 
-            rip_response = self.server_obj.step(RipRequest(value=0))
+            rip_response = self.server_obj.step(RipRequest(value=action))
             self.motor_position = math.radians(rip_response.arm_angle)
             self.motor_velocity = rip_response.arm_velocity
             self.pendulum_1_position = math.radians(rip_response.link_1_angle)
@@ -610,9 +610,9 @@ class RotaryInvertedPendulumEnv(gym.Env):
             self.pendulum_2_velocity = rip_response.link_2_velocity
             self.simulation_time = None
             # print("spi link_1 angle : {0:5.3f}".format(rip_response.link_1_angle))
-            print("motor vel :{0:5.3f}, pen1_Vel : {1:5.3f}, pen2_vel : {2:5.3f}, motor posi :{3:5.3f}, pen1 posi :{4:5.3f}, pen2 posi :{5:5.3f}".format(
-                self.motor_velocity, self.pendulum_1_velocity, self.pendulum_2_velocity, self.motor_velocity, rip_response.link_1_angle, rip_response.link_2_angle
-            ))
+            # print("motor vel :{0:5.3f}, pen1_Vel : {1:5.3f}, pen2_vel : {2:5.3f}, motor posi :{3:5.3f}, pen1 posi :{4:5.3f}, pen2 posi :{5:5.3f}".format(
+            #     self.motor_velocity, self.pendulum_1_velocity, self.pendulum_2_velocity, self.motor_velocity, rip_response.link_1_angle, rip_response.link_2_angle
+            # ))
             # time.sleep(0.5)
             # if rip_response.message == "FORCE_TERMINATE":
             #     print("FORCE TERMINATE !!!!!!!!!!!!!!!!!!")
