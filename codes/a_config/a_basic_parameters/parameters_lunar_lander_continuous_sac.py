f3aa5979f3d2ec196474a4f61cd37bd0560ea555
Youn-Hee Han <yh21.han@gmail.com>
2021-06-25 21:16:43 +0900

    ...

---
+++ codes/a_config/a_basic_parameters/parameters_lunar_lander_continuous_sac.py
@@ -0,0 +1,25 @@
+from codes.a_config._rl_parameters.off_policy.parameter_sac import PARAMETERS_SAC
+from codes.a_config._rl_parameters.on_policy.parameter_ppo import PARAMETERS_PPO
+from codes.e_utils.names import *
+from codes.a_config.parameters_general import PARAMETERS_GENERAL
+
+
+class PARAMETERS_LUNAR_LANDER_CONTINUOUS_SAC(PARAMETERS_GENERAL, PARAMETERS_SAC):
+    ENVIRONMENT_ID = EnvironmentName.LUNAR_LANDER_CONTINUOUS_V2
+    DEEP_LEARNING_MODEL = DeepLearningModelName.SOFT_ACTOR_CRITIC_MLP
+    RL_ALGORITHM = RLAlgorithmName.SAC_V0
+    OPTIMIZER = OptimizerName.ADAM
+
+    TRAIN_STOP_EPISODE_REWARD = 180.0
+    STOP_PATIENCE_COUNT = 10
+
+    MAX_GLOBAL_STEP = 10000000
+    GAMMA = 0.99
+    BATCH_SIZE = 32
+    AVG_EPISODE_SIZE_FOR_STAT = 50
+
+    CLIP_GRAD = 3.0
+
+    ACTOR_LEARNING_RATE = 0.0002
+    LEARNING_RATE = 0.001
+    N_STEP = 2
