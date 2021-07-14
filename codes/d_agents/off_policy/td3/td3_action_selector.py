0f5034fec4884c19cb39d7fd6b76489f19b92fc8
Youn-Hee Han <yh21.han@gmail.com>
2021-07-14 18:40:16 +0900

    td3_action moving_average 기능 주석처리

--- codes/d_agents/off_policy/td3/td3_action_selector.py
+++ codes/d_agents/off_policy/td3/td3_action_selector.py
@@ -14,20 +14,20 @@ class TD3ActionSelector:
         self.noise_std = self.original_noise_std = noise_std
         self.epsilon = epsilon
         self.params = params
-        self.action_list = deque(maxlen=11)
+        # self.action_list = deque(maxlen=11)
 
     def select_action(self, mu):
         actions = np.copy(mu)
 
-        self.action_list.append(actions[0][0])
-
-        actions = np.expand_dims(
-            np.expand_dims(
-                np.asarray(ema(list(self.action_list), len(self.action_list) - 1)[-1]),
-                axis=-1
-            ),
-            axis=-1
-        )
+        # self.action_list.append(actions[0][0])
+        #
+        # actions = np.expand_dims(
+        #     np.expand_dims(
+        #         np.asarray(ema(list(self.action_list), len(self.action_list) - 1)[-1]),
+        #         axis=-1
+        #     ),
+        #     axis=-1
+        # )
 
         if self.noise_std == 0.0:
             noises = np.zeros(shape=actions.shape)
@@ -52,7 +52,6 @@ class TD3ActionSelector:
         assert isinstance(mu, np.ndarray)
         actions = np.copy(mu)
         actions, noises = self.select_action(actions)
-        #ic(noises)
 
         return actions, noises
 
