2c1960d50a062a04c397d73b0b5ee0cc5edf2210
Youn-Hee Han <yh21.han@gmail.com>
2021-12-13 10:53:05 +0900

    ....

--- a_configuration/parameters/parameter.template.py
+++ a_configuration/parameters/parameter.template.py
@@ -1,6 +1,29 @@
 from a_configuration.parameters.parameter_preamble import *
 
+###############
+## CART_POLE ##
+###############
+# class Parameter(ParameterCartPoleDqn):
+#     USE_WANDB = False
+#     WANDB_ENTITY = "link-koreatech"
 
-class Parameter(ParameterCartPoleDqn):
+# class Parameter(ParameterCartPoleReinforce):
+#     USE_WANDB = False
+#     WANDB_ENTITY = "link-koreatech"
+
+# class Parameter(ParameterCartPoleA2c):
+#     USE_WANDB = False
+#     WANDB_ENTITY = "link-koreatech"
+
+
+##########
+## PONG ##
+##########
+class Parameter(ParameterPongDqn):
     USE_WANDB = False
-    WANDB_ENTITY = ""
+    WANDB_ENTITY = "link-koreatech"
+
+
+if __name__ == "__main__":
+    parameter = Parameter()
+    print_basic_info(device=None, parameter=parameter)
