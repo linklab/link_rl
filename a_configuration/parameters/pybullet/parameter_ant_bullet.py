28ceb56166c85dd8c632de55607930735fa62e36
Youn-Hee Han <yh21.han@gmail.com>
2021-12-19 08:11:57 +0900

    ...

--- a_configuration/parameters/pybullet/parameter_ant_bullet.py
+++ a_configuration/parameters/pybullet/parameter_ant_bullet.py
@@ -16,7 +16,7 @@ class ParameterAntBulletA2c(
 
         self.N_VECTORIZED_ENVS = 1
         self.N_ACTORS = 1
-        self.MAX_TRAINING_STEPS = 100_000
+        self.MAX_TRAINING_STEPS = 1_000_000
         self.CONSOLE_LOG_INTERVAL_TRAINING_STEPS = 100
         self.TEST_INTERVAL_TRAINING_STEPS = 1_024
         self.BUFFER_CAPACITY = self.BATCH_SIZE
