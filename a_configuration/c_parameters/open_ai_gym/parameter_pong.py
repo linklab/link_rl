795b5bb261c26314a167a43cd17f12baf65015c8
Youn-Hee Han <yh21.han@gmail.com>
2021-12-26 09:37:24 +0900

    ...

--- a_configuration/c_parameters/open_ai_gym/parameter_pong.py
+++ a_configuration/c_parameters/open_ai_gym/parameter_pong.py
@@ -30,14 +30,14 @@ class ParameterPongA2c(
         ParameterPong.__init__(self)
         ParameterA2c.__init__(self)
 
-        self.BATCH_SIZE = 128
-        self.LEARNING_RATE = 0.001
-        self.N_STEP = 4
+        self.BATCH_SIZE = 64
+        self.LEARNING_RATE = 0.0001
+        self.N_STEP = 1
         self.N_VECTORIZED_ENVS = 2
         self.N_ACTORS = 1
         self.MAX_TRAINING_STEPS = 1_000_000
         self.CONSOLE_LOG_INTERVAL_TRAINING_STEPS = 100
-        self.TEST_INTERVAL_TRAINING_STEPS = 1024
+        self.TEST_INTERVAL_TRAINING_STEPS = 1_024
         self.BUFFER_CAPACITY = self.BATCH_SIZE
         self.MODEL = ParameterConvolutionalModel(ModelType.MEDIUM_CONVOLUTIONAL)
 
