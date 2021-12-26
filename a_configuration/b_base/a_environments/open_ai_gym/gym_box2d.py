da1c2fd94d81a8782c2b860ce496e19b333942a4
Youn-Hee Han <yh21.han@gmail.com>
2021-12-27 07:18:44 +0900

    ...

--- a_configuration/b_base/a_environments/open_ai_gym/gym_box2d.py
+++ a_configuration/b_base/a_environments/open_ai_gym/gym_box2d.py
@@ -2,7 +2,7 @@ class ParameterLunarLander:
     def __init__(self):
         self.ENV_NAME = "LunarLander-v2"
         self.EPISODE_REWARD_AVG_SOLVED = 190
-        self.EPISODE_REWARD_STD_SOLVED = 10.0
+        self.EPISODE_REWARD_STD_SOLVED = 25.0
         self.TEST_INTERVAL_TRAINING_STEPS = 1024
 
 
@@ -10,5 +10,5 @@ class ParameterLunarLanderContinuous:
     def __init__(self):
         self.ENV_NAME = "LunarLanderContinuous-v2"
         self.EPISODE_REWARD_AVG_SOLVED = 190
-        self.EPISODE_REWARD_STD_SOLVED = 10.0
+        self.EPISODE_REWARD_STD_SOLVED = 25.0
         self.TEST_INTERVAL_TRAINING_STEPS = 1024
