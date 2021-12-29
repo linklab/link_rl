486d09505ba7cec4be5b3d9b26e9edfe847632a6
Youn-Hee Han <yh21.han@gmail.com>
2021-12-30 00:52:49 +0900

    actor_critic에 대한 wandb 그래프 추가

--- d_agents/off_policy/ddpg/agent_ddpg.py
+++ d_agents/off_policy/ddpg/agent_ddpg.py
@@ -47,23 +47,24 @@ class AgentDdpg(Agent):
         else:
             raise ValueError()
 
-        self.ddpg_model.actor_model.share_memory()
-        self.ddpg_model.critic_model.share_memory()
-
-        self.synchronize_models(
-            source_model=self.ddpg_model.actor_model, target_model=self.target_ddpg_model.actor_model
-        )
-        self.synchronize_models(
-            source_model=self.ddpg_model.critic_model, target_model=self.target_ddpg_model.critic_model
-        )
-
-        self.actor_optimizer = optim.Adam(self.ddpg_model.actor_model.actor_params, lr=self.parameter.LEARNING_RATE)
-        self.critic_optimizer = optim.Adam(self.ddpg_model.critic_model.critic_params, lr=self.parameter.LEARNING_RATE)
-
         self.model = self.ddpg_model.actor_model
-
         self.actor_model = self.ddpg_model.actor_model
         self.critic_model = self.ddpg_model.critic_model
+        self.target_actor_model = self.target_ddpg_model.actor_model
+        self.target_critic_model = self.target_ddpg_model.critic_model
+
+        self.actor_model.share_memory()
+        self.critic_model.share_memory()
+
+        self.synchronize_models(
+            source_model=self.actor_model, target_model=self.target_actor_model
+        )
+        self.synchronize_models(
+            source_model=self.critic_model, target_model=self.target_critic_model
+        )
+
+        self.actor_optimizer = optim.Adam(self.actor_model.actor_params, lr=self.parameter.LEARNING_RATE)
+        self.critic_optimizer = optim.Adam(self.critic_model.critic_params, lr=self.parameter.LEARNING_RATE)
 
         self.training_steps = 0
 
@@ -71,7 +72,7 @@ class AgentDdpg(Agent):
         self.last_actor_loss = mp.Value('d', 0.0)
 
     def get_action(self, obs, mode=AgentMode.TRAIN):
-        mu = self.ddpg_model.actor_model.pi(obs)
+        mu = self.actor_model.pi(obs)
         mu = mu.detach()
         if mode == AgentMode.TRAIN:
             noises = np.random.normal(size=self.n_actions, loc=0, scale=1.0)
@@ -95,13 +96,13 @@ class AgentDdpg(Agent):
         #######################
         # train actor - BEGIN #
         #######################
-        mu_v = self.ddpg_model.actor_model.pi(observations)
-        q_v = self.ddpg_model.critic_model.q(observations, mu_v)
+        mu_v = self.actor_model.pi(observations)
+        q_v = self.critic_model.q(observations, mu_v)
         actor_loss = -1.0 * q_v.mean()
 
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
-        torch.nn.utils.clip_grad_value_(self.ddpg_model.actor_model.actor_params, self.parameter.CLIP_GRADIENT_VALUE)
+        torch.nn.utils.clip_grad_value_(self.actor_model.actor_params, self.parameter.CLIP_GRADIENT_VALUE)
         self.actor_optimizer.step()
         #####################
         # train actor - END #
@@ -111,18 +112,18 @@ class AgentDdpg(Agent):
         # train critic - BEGIN #
         ########################
         with torch.no_grad():
-            next_mu_v = self.target_ddpg_model.actor_model.pi(next_observations)
-            next_q_v = self.target_ddpg_model.critic_model.q(next_observations, next_mu_v)
+            next_mu_v = self.target_actor_model.pi(next_observations)
+            next_q_v = self.target_critic_model.q(next_observations, next_mu_v)
             next_q_v[dones] = 0.0
             target_q_v = rewards + self.parameter.GAMMA ** self.parameter.N_STEP * next_q_v
 
-        q_v = self.ddpg_model.critic_model.q(observations, actions)
+        q_v = self.critic_model.q(observations, actions)
 
         critic_loss = F.mse_loss(q_v, target_q_v.detach())
 
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
-        torch.nn.utils.clip_grad_value_(self.ddpg_model.critic_model.critic_params, self.parameter.CLIP_GRADIENT_VALUE)
+        torch.nn.utils.clip_grad_value_(self.critic_model.critic_params, self.parameter.CLIP_GRADIENT_VALUE)
         self.critic_optimizer.step()
         ######################
         # train critic - end #
@@ -130,14 +131,12 @@ class AgentDdpg(Agent):
 
         # TAU: 0.0001
         self.soft_synchronize_models(
-            source_model=self.ddpg_model.actor_model, target_model=self.target_ddpg_model.actor_model,
-            tau=self.parameter.TAU
+            source_model=self.actor_model, target_model=self.target_actor_model, tau=self.parameter.TAU
         )
 
         # TAU: 0.0001
         self.soft_synchronize_models(
-            source_model=self.ddpg_model.critic_model, target_model=self.target_ddpg_model.critic_model,
-            tau=self.parameter.TAU
+            source_model=self.critic_model, target_model=self.target_critic_model, tau=self.parameter.TAU
         )
 
         self.last_critic_loss.value = critic_loss.item()
