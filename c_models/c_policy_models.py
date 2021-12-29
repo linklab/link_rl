6ae5e568113b8d18a32b247e0b28bda26910dddf
zzzanghun <56795273+zzzanghun@users.noreply.github.com>
2021-12-29 23:34:10 +0900

    parent class init 받아오는 방법 수정

--- c_models/c_policy_models.py
+++ c_models/c_policy_models.py
@@ -58,7 +58,7 @@ class DiscretePolicyModel(PolicyModel):
             self, observation_shape: Tuple[int], n_out_actions: int, n_discrete_actions=None,
             device=torch.device("cpu"), parameter=None
     ):
-        super(DiscretePolicyModel, self).__init__(observation_shape, n_out_actions, n_discrete_actions, device, parameter)
+        PolicyModel.__init__(self, observation_shape, n_out_actions, n_discrete_actions, device, parameter)
 
         self.actor_fc_pi = nn.Linear(self.parameter.MODEL.NEURONS_PER_FULLY_CONNECTED_LAYER[-1], self.n_discrete_actions)
         self.actor_params += list(self.actor_fc_pi.parameters())
@@ -74,9 +74,11 @@ class ContinuousPolicyModel(PolicyModel):
     def __init__(
             self, observation_shape: Tuple[int], n_out_actions: int, device=torch.device("cpu"), parameter=None
     ):
-        super(ContinuousPolicyModel, self).__init__(
-            observation_shape=observation_shape, n_out_actions=n_out_actions, device=device, parameter=parameter
+        PolicyModel.__init__(
+            self, observation_shape=observation_shape, n_out_actions=n_out_actions, device=device, parameter=parameter
         )
+
+
 
         self.mu = nn.Sequential(
             nn.Linear(self.parameter.MODEL.NEURONS_PER_FULLY_CONNECTED_LAYER[-1], self.n_out_actions),
