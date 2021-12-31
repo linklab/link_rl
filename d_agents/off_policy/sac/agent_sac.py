c57dd8e01de2fe0791c289db5aa5371090368e7c
zzzanghun <56795273+zzzanghun@users.noreply.github.com>
2021-12-31 17:23:37 +0900

    sac gradient clip 방식 변경

--- d_agents/off_policy/sac/agent_sac.py
+++ d_agents/off_policy/sac/agent_sac.py
@@ -140,7 +140,7 @@ class AgentSac(Agent):
 
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
-        torch.nn.utils.clip_grad_value_(self.critic_model.critic_params, self.parameter.CLIP_GRADIENT_VALUE)
+        torch.nn.utils.clip_grad_norm_(self.critic_model.critic_params, self.parameter.CLIP_GRADIENT_VALUE)
         self.critic_optimizer.step()
         ###################################
         #  Critic (Value)  Loss 산출 - END #
@@ -156,7 +156,7 @@ class AgentSac(Agent):
 
         self.actor_optimizer.zero_grad()
         loss_actor_v.backward()
-        torch.nn.utils.clip_grad_value_(self.actor_model.actor_params, self.parameter.CLIP_GRADIENT_VALUE)
+        torch.nn.utils.clip_grad_norm_(self.actor_model.actor_params, self.parameter.CLIP_GRADIENT_VALUE)
         self.actor_optimizer.step()
         ##############################
         #  Actor Objective 산출 - END #
