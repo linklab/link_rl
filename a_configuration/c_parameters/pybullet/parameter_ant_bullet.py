db32f8561e533bbdc4b1d029554c37e2b4899150
Youn-Hee Han (iMAC) <yh21.han@gmail.com>
2022-01-01 20:04:57 +0900

    ...

--- a_configuration/c_parameters/pybullet/parameter_ant_bullet.py
+++ a_configuration/c_parameters/pybullet/parameter_ant_bullet.py
@@ -57,7 +57,7 @@ class ParameterAntBulletSac(ParameterBase, ParameterAntBullet, ParameterSac):
 
         self.N_VECTORIZED_ENVS = 1
         self.N_ACTORS = 1
-        self.MAX_TRAINING_STEPS = 500_000
+        self.MAX_TRAINING_STEPS = 2_000_000
         self.CONSOLE_LOG_INTERVAL_TRAINING_STEPS = 100
         self.TEST_INTERVAL_TRAINING_STEPS = 5_000
         self.MODEL = ParameterLinearModel(ModelType.SMALL_LINEAR)
