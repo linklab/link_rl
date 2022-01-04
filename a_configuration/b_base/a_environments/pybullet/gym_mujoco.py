1942916e5e7c8cf2916f4177caa1e4c35e0bd628
zzzanghun <56795273+zzzanghun@users.noreply.github.com>
2022-01-05 01:53:51 +0900

    walker2d 파라미터 추가

--- a_configuration/b_base/a_environments/pybullet/gym_mujoco.py
+++ a_configuration/b_base/a_environments/pybullet/gym_mujoco.py
@@ -12,3 +12,11 @@ class ParameterHopperMujoco:
         self.EPISODE_REWARD_AVG_SOLVED = 3000
         self.EPISODE_REWARD_STD_SOLVED = 300
         self.TEST_INTERVAL_TRAINING_STEPS = 1_024
+
+
+class ParameterWalker2dMujoco:
+    def __init__(self):
+        self.ENV_NAME = "Walker2d-v2"
+        self.EPISODE_REWARD_AVG_SOLVED = 5000
+        self.EPISODE_REWARD_STD_SOLVED = 300
+        self.TEST_INTERVAL_TRAINING_STEPS = 1_024
