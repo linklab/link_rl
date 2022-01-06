c6610e9df3d9777a6e523e74eef92edd0795ef4a
zzzanghun <56795273+zzzanghun@users.noreply.github.com>
2022-01-06 16:18:29 +0900

    comparison parameter mujoco hopper alpha type 추가

--- a_configuration/b_parameter_comparison_template.py
+++ a_configuration/b_parameter_comparison_template.py
@@ -1,3 +1,5 @@
+from a_configuration.d_parameters_comparison.mujoco.parameter_comparison_hopper import \
+    ParameterComparisonMujocoHopperSac
 from a_configuration.d_parameters_comparison.open_ai_gym.parameter_comparison_cartpole import \
     ParameterComparisonCartPoleDqn, ParameterComparisonCartPoleDqnTypes
 from a_configuration.d_parameters_comparison.open_ai_gym.parameter_comparison_pong import ParameterComparisonPongDqn
@@ -62,6 +64,22 @@ parameter_comparison_cart_pole_dqn_types.AGENT_LABELS = [
 parameter_comparison_cart_pole_dqn_types.MAX_TRAINING_STEPS = 50_000
 parameter_comparison_cart_pole_dqn_types.N_RUNS = 5
 parameter_comparison_list.append(parameter_comparison_cart_pole_dqn_types)
+
+######################################################################
+
+parameter_comparison_mujoco_hopper_alpha = ParameterComparisonMujocoHopperSac()
+parameter_comparison_mujoco_hopper_alpha.AGENT_PARAMETERS[0].AUTOMATIC_ENTROPY_TEMPERATURE_TUNING = False
+parameter_comparison_mujoco_hopper_alpha.AGENT_PARAMETERS[0].DEFAULT_ALPHA = 0.2
+parameter_comparison_mujoco_hopper_alpha.AGENT_PARAMETERS[1].AUTOMATIC_ENTROPY_TEMPERATURE_TUNING = False
+parameter_comparison_mujoco_hopper_alpha.AGENT_PARAMETERS[1].DEFAULT_ALPHA = 0.5
+parameter_comparison_cart_pole_dqn_types.AGENT_LABELS = [
+    "alpha = 0.2",
+    "alpha = 0.5",
+    "alpha tuning",
+]
+parameter_comparison_cart_pole_dqn_types.MAX_TRAINING_STEPS = 300000
+parameter_comparison_cart_pole_dqn_types.N_RUNS = 5
+parameter_comparison_list.append(parameter_comparison_mujoco_hopper_alpha)
 
 #######################################################################
 for parameter_comparison in parameter_comparison_list:
---
+++ a_configuration/d_parameters_comparison/mujoco/parameter_comparison_hopper.py
@@ -0,0 +1,15 @@
+from a_configuration.b_base.parameter_base_comparison import ParameterComparisonBase
+from a_configuration.c_parameters.mujoco.parameter_hopper_mujoco import ParameterHopperMujocoSac
+
+
+class ParameterComparisonMujocoHopperSac(ParameterComparisonBase):
+    def __init__(self):
+        ParameterComparisonBase.__init__(self)
+
+        self.ENV_NAME = "Hopper-v2"
+
+        self.AGENT_PARAMETERS = [
+            ParameterHopperMujocoSac(),
+            ParameterHopperMujocoSac(),
+            ParameterHopperMujocoSac()
+        ]
