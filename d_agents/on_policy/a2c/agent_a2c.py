84eedea4333e93397bc55fe706103a3a8b615f5c
Youn-Hee Han <yh21.han@gmail.com>
2022-02-07 08:33:50 +0900

    a2c, sac, ppo bug fix

--- d_agents/on_policy/a2c/agent_a2c.py
+++ d_agents/on_policy/a2c/agent_a2c.py
@@ -58,14 +58,14 @@ class AgentA2c(OnPolicyAgent):
             return action
 
         elif isinstance(self.action_space, Box):
-            mu_v, sigma_v = self.actor_model.pi(obs)
+            mu_v, var_v = self.actor_model.pi(obs)
 
             if mode == AgentMode.TRAIN:
                 # actions = np.random.normal(
                 #     loc=mu_v.detach().cpu().numpy(), scale=torch.sqrt(var_v).detach().cpu().numpy()
                 # )
 
-                dist = Normal(loc=mu_v, scale=sigma_v)
+                dist = Normal(loc=mu_v, scale=torch.sqrt(var_v))
                 actions = dist.sample().detach().cpu().numpy()
             else:
                 actions = mu_v.detach().cpu().numpy()
@@ -141,13 +141,13 @@ class AgentA2c(OnPolicyAgent):
 
             entropy = dist.entropy().mean()
         elif isinstance(self.action_space, Box):
-            mu_v, sigma_v = self.actor_model.pi(self.observations)
+            mu_v, var_v = self.actor_model.pi(self.observations)
 
             # criticized_log_pi_action_v = self.calc_log_prob(mu_v, var_v, self.actions) * advantages
             # entropy = 0.5 * (torch.log(2.0 * np.pi * var_v) + 1.0).sum(dim=-1)
             # entropy = entropy.mean()
 
-            dist = Normal(loc=mu_v, scale=sigma_v)
+            dist = Normal(loc=mu_v, scale=torch.sqrt(var_v))
             criticized_log_pi_action_v = dist.log_prob(value=self.actions).sum(dim=-1, keepdim=True) * advantages
             entropy = dist.entropy().mean()
         else:
