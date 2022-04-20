5475e7a2e6cc3fbda19a18b5abb7219d3246550e
Youn-Hee Han (iMAC) <yh21.han@gmail.com>
2022-04-20 12:31:30 +0900

    PONG Comparison Config Updated

--- a_configuration/a_single_config_template.py
+++ a_configuration/a_single_config_template.py
@@ -272,6 +272,7 @@ config = ConfigPongDuelingDqn()
 
 from a_configuration.b_single_config.open_ai_gym.atari.config_pong import ConfigPongDoubleDuelingDqn
 config = ConfigPongDoubleDuelingDqn()
+config.WANDB_REPORT_URL = "https://wandb.ai/link-koreatech/Pong-v5_DOUBLE_DUELING_DQN_yhhan/reports/Pong-v5_DOUBLE_DUELING_DQN--VmlldzoxODYxMjU4?accessToken=gsvpknqu1gqi6cb0177jgdzk61oqhpua4kcj680l89fdvwer1cg3wms7rb62syj8"
 
 from a_configuration.b_single_config.open_ai_gym.atari.config_pong import ConfigPongA2c
 config = ConfigPongA2c()
