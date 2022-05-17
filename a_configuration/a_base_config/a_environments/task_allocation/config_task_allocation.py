b126f7f38ba1e6d3b3a6a30f6c9003c3691b93ca
z3rO1997 <46732343+z3rO1997@users.noreply.github.com>
2022-05-17 23:10:06 +0900

    fix task allocation

--- a_configuration/a_base_config/a_environments/task_allocation/config_task_allocation.py
+++ a_configuration/a_base_config/a_environments/task_allocation/config_task_allocation.py
@@ -26,8 +26,8 @@ class ConfigTaskAllocation:
         #training setting
         self.MAX_TRAINING_STEPS = self.NUM_TASK * 2_000
         self.BUFFER_CAPACITY = self.NUM_TASK * 1_000
-        self.EPISODE_REWARD_AVG_SOLVED = 100
-        self.EPISODE_REWARD_STD_SOLVED = 20
+        self.EPISODE_REWARD_AVG_SOLVED = 1000000
+        self.EPISODE_REWARD_STD_SOLVED = 1
 
         self.CONSOLE_LOG_INTERVAL_TRAINING_STEPS = 50
         self.MODEL_TYPE = ModelType.SMALL_LINEAR
