302fea3fd48bd7b9037eb6ee1c87f59ff0741ce8
Youn-Hee Han <yh21.han@gmail.com>
2022-05-21 01:42:17 +0900

    import fix

--- g_utils/commons.py
+++ g_utils/commons.py
@@ -815,11 +815,11 @@ def get_train_env(config, no_graphics=True):
                 env = TaskAllocationEnvironment(config)
 
             elif config.ENV_NAME in ["Knapsack_Problem_v0"]:
-                from b_environments.combinatorial_optimization.knapsack import KnapsackEnv
+                from b_environments.combinatorial_optimization.knapsack.knapsack import KnapsackEnv
                 env = KnapsackEnv(config)
 
             elif config.ENV_NAME in ["Knapsack_Problem_v1"]:
-                from b_environments.combinatorial_optimization.knapsack_actions import KnapsackEnv
+                from b_environments.combinatorial_optimization.knapsack.knapsack_actions import KnapsackEnv
                 env = KnapsackEnv(config)
 
             #############
