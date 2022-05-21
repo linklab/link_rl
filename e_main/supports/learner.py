02b52caa81c5ec4d1fdda8999cf7f0581c6a8051
Youn-Hee Han <yh21.han@gmail.com>
2022-05-21 13:10:28 +0900

    Knapsack_Problem_v1 제거 및 Her_Knapsack_Problem_v0

--- e_main/supports/learner.py
+++ e_main/supports/learner.py
@@ -56,7 +56,7 @@ class Learner(mp.Process):
         if config.ENV_NAME in ["Task_Allocation_v0"]:
             self.test_episode_utilization = mp.Value('d', 0.0)
 
-        if config.ENV_NAME in ["Knapsack_Problem_v0", "Knapsack_Problem_v1"]:
+        if config.ENV_NAME in ["Knapsack_Problem_v0", "Her_Knapsack_Problem_v0"]:
             self.test_episode_items_value = mp.Value('d', 0.0)
 
         self.next_train_time_step = config.TRAIN_INTERVAL_GLOBAL_TIME_STEPS
@@ -88,7 +88,7 @@ class Learner(mp.Process):
 
         self.shared_model_access_lock = shared_model_access_lock  # For only LearningActor (A3C)
 
-        if self.config.ENV_NAME in ["Task_Allocation_v0", "Task_Allocation_v1", "Knapsack_Problem_v0", "Knapsack_Problem_v1"]:
+        if self.config.ENV_NAME in ["Task_Allocation_v0", "Task_Allocation_v1", "Knapsack_Problem_v0", "Her_Knapsack_Problem_v0"]:
             self.env_info = None
 
         self.modified_env_name = self.config.ENV_NAME.split("/")[1] if "/" in self.config.ENV_NAME else self.config.ENV_NAME
@@ -125,7 +125,7 @@ class Learner(mp.Process):
 
             next_observations, rewards, dones, infos = self.train_env.step(scaled_actions)
 
-            if self.config.ENV_NAME in ["Task_Allocation_v0", "Knapsack_Problem_v0", "Knapsack_Problem_v1"]:
+            if self.config.ENV_NAME in ["Task_Allocation_v0", "Knapsack_Problem_v0", "Her_Knapsack_Problem_v0"]:
                 self.env_info = infos[0]
 
             if self.config.ENV_NAME in ["Task_Allocation_v1"]:
@@ -441,7 +441,7 @@ class Learner(mp.Process):
         if self.config.ENV_NAME in ["Task_Allocation_v0"]:
             self.test_episode_reward_avg.value, self.test_episode_reward_std.value, self.test_episode_utilization.value = \
                 self.play_for_testing(self.config.N_TEST_EPISODES)
-        elif self.config.ENV_NAME in ["Knapsack_Problem_v0", "Knapsack_Problem_v1"]:
+        elif self.config.ENV_NAME in ["Knapsack_Problem_v0", "Her_Knapsack_Problem_v0"]:
             self.test_episode_reward_avg.value, self.test_episode_reward_std.value, self.test_episode_items_value.value = \
                 self.play_for_testing(self.config.N_TEST_EPISODES)
         else:
@@ -461,7 +461,7 @@ class Learner(mp.Process):
 
         if self.config.ENV_NAME in ["Task_Allocation_v0"]:
             test_str += ", Utilization: {0:.2f}".format(self.test_episode_utilization.value)
-        if self.config.ENV_NAME in ["Knapsack_Problem_v0", "Knapsack_Problem_v1"]:
+        if self.config.ENV_NAME in ["Knapsack_Problem_v0", "Her_Knapsack_Problem_v0"]:
             test_str += ", Values: {0:.2f}".format(self.test_episode_items_value.value)
 
         test_str += ", Elapsed Time from Training Start: {0}".format(formatted_elapsed_time)
@@ -502,7 +502,7 @@ class Learner(mp.Process):
         episode_reward_lst = []
         if self.config.ENV_NAME in ["Task_Allocation_v0"]:
             episode_utilization_lst = []
-        elif self.config.ENV_NAME in ["Knapsack_Problem_v0", "Knapsack_Problem_v1"]:
+        elif self.config.ENV_NAME in ["Knapsack_Problem_v0", "Her_Knapsack_Problem_v0"]:
             episode_value_lst = []
 
         for i in range(n_test_episodes):
@@ -596,7 +596,7 @@ class Learner(mp.Process):
 
         if self.config.ENV_NAME in ["Task_Allocation_v0"]:
             return np.average(episode_reward_lst), np.std(episode_reward_lst), np.average(episode_utilization_lst)
-        elif self.config.ENV_NAME in ["Knapsack_Problem_v0","Knapsack_Problem_v1"]:
+        elif self.config.ENV_NAME in ["Knapsack_Problem_v0", "Her_Knapsack_Problem_v0"]:
             return np.average(episode_reward_lst), np.std(episode_reward_lst), np.average(episode_value_lst)
         else:
             return np.average(episode_reward_lst), np.std(episode_reward_lst)
