5583750ea2a4b4ce6bc0d331d390004e91c869fb
DocyNoah <yoweifc@gmail.com>
2022-07-01 23:55:14 +0900

    Add QModelCreatorConv

--- link_rl/c_models_v2/b_q_model_creator.py
+++ link_rl/c_models_v2/b_q_model_creator.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from typing import final, Tuple
+from typing import final, Tuple, List, Union, cast
 
 from link_rl.c_models_v2.a_model_creator import SingleModelCreator, model_creator_registry
 
@@ -82,3 +82,72 @@ class DuelingQModelCreator(SingleModelCreator):
 
         dueling_q_model = DuelingQModel()
         return dueling_q_model
+
+
+@model_creator_registry.add
+class QModelCreatorConv(SingleModelCreator):
+    name = "QModelCreatorConv"
+
+    def __init__(
+        self,
+        observation_shape: Tuple[int, ...],
+        n_out_actions: int,
+        n_discrete_actions=None
+    ):
+        super(QModelCreatorConv, self).__init__(
+            observation_shape,
+            n_out_actions,
+            n_discrete_actions
+        )
+
+    @final
+    def _create_model(self) -> nn.Module:
+        def make_vgg_layers(in_channels: int, config: List[Union[str, int]]) -> nn.Sequential:
+            layers: List[nn.Module] = []
+            _in_channels = in_channels
+            for v in config:
+                if v == "M":
+                    layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+                else:
+                    channels = cast(int, v)
+                    conv2d = nn.Conv2d(
+                        in_channels=_in_channels,
+                        out_channels=channels,
+                        kernel_size=3,
+                        padding=1
+                    )
+                    layers += [conv2d, nn.ReLU(inplace=True)]
+                    _in_channels = channels
+            return nn.Sequential(*layers)
+
+        vgg16_layers = [64, 64, "M", 128, 128, "M", 256, 256, 256, "M", 512, 512, 512, "M", 512, 512, 512, "M"]
+        vgg16_net = make_vgg_layers(
+            in_channels=self._n_input,
+            config=vgg16_layers
+        )
+
+        conv_out = self._get_conv_out(
+            conv_layers=vgg16_net,
+            shape=self._observation_shape
+        )
+
+        linear_model = nn.Sequential(
+            nn.Linear(conv_out, 512),
+            nn.LayerNorm(512),
+            nn.LeakyReLU(),
+            nn.Linear(512, 256),
+            nn.LayerNorm(256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 128),
+            nn.LayerNorm(128),
+            nn.LeakyReLU(),
+            nn.Linear(128, self._n_discrete_actions)
+        )
+
+        model = nn.Sequential(
+            vgg16_net,
+            nn.Flatten(start_dim=1),
+            linear_model
+        )
+
+        return model
