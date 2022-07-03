d581bd68bb8cf2586f459aa429620cd5f9b2a9e7
Youn-Hee Han <yh21.han@gmail.com>
2022-07-03 23:30:05 +0900

    ActorModel 및 CriticModel 등을 Inner 클래스로 Scope 상향

--- link_rl/c_models_v2/g_sac_model_creator.py
+++ link_rl/c_models_v2/g_sac_model_creator.py
@@ -95,6 +95,42 @@ class ContinuousSacModelCreator(DoubleModelCreator):
 class ContinuousEncoderSacModelCreator(DoubleModelCreator):
     name = "ContinuousEncoderSacModelCreator"
 
+    class ActorModel(nn.Module):
+        def __init__(self, encoder_net, shared_net, actor_net, actor_mu_net, actor_var_net):
+            super().__init__()
+            self.encoder_net = encoder_net
+            self.shared_net = shared_net
+            self.actor_net = actor_net
+            self.actor_mu_net = actor_mu_net
+            self.actor_var_net = actor_var_net
+
+        def forward(self, obs):
+            x = self.encoder_net(obs)
+            x = self.shared_net(x)
+            x = self.actor_net(x)
+            mu = self.actor_mu_net(x)
+            var = self.actor_var_net(x)
+            return mu, var
+
+    class CriticModel(nn.Module):
+        def __init__(self, encoder_net, shared_net, critic_net, q1_critic_net, q2_critic_net):
+            super().__init__()
+            self.encoder_net = encoder_net
+            self.shared_net = shared_net
+            self.critic_net = critic_net
+            self.q1_critic_net = q1_critic_net
+            self.q2_critic_net = q2_critic_net
+
+        def forward(self, obs, action):
+            x = self.encoder_net(obs)
+            x = self.shared_net(x)
+            x = torch.cat([x, action], dim=-1).float()
+            x = self.critic_net(x)
+            q1 = self.q1_critic_net(x)
+            q2 = self.q2_critic_net(x)
+
+            return q1, q2
+
     def __init__(
         self,
         observation_shape: Tuple[int, ...],
@@ -160,42 +196,10 @@ class ContinuousEncoderSacModelCreator(DoubleModelCreator):
         q1_critic_net = nn.Linear(128, 1)
         q2_critic_net = nn.Linear(128, 1)
 
-        class ActorModel(nn.Module):
-            def __init__(self):
-                super().__init__()
-                self.encoder_net = encoder_net
-                self.shared_net = shared_net
-                self.actor_net = actor_net
-                self.actor_mu_net = actor_mu_net
-                self.actor_var_net = actor_var_net
-
-            def forward(self, obs):
-                x = self.encoder_net(obs)
-                x = self.shared_net(x)
-                x = self.actor_net(x)
-                mu = self.actor_mu_net(x)
-                var = self.actor_var_net(x)
-                return mu, var
-
-        class CriticModel(nn.Module):
-            def __init__(self):
-                super().__init__()
-                self.encoder_net = encoder_net
-                self.shared_net = shared_net
-                self.critic_net = critic_net
-                self.q1_critic_net = q1_critic_net
-                self.q2_critic_net = q2_critic_net
-
-            def forward(self, obs, action):
-                x = self.encoder_net(obs)
-                x = self.shared_net(x)
-                x = torch.cat([x, action], dim=-1).float()
-                x = self.critic_net(x)
-                q1 = self.q1_critic_net(x)
-                q2 = self.q2_critic_net(x)
-
-                return q1, q2
-
-        actor_model = ActorModel()
-        critic_model = CriticModel()
+        actor_model = ContinuousEncoderSacModelCreator.ActorModel(
+            encoder_net, shared_net, actor_net, actor_mu_net, actor_var_net
+        )
+        critic_model = ContinuousEncoderSacModelCreator.CriticModel(
+            encoder_net, shared_net, critic_net, q1_critic_net, q2_critic_net
+        )
         return actor_model, critic_model
