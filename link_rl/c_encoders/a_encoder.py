3e5bbee3002e7cc3a956340b01fa5f1bbd5f634d
zzzanghun <wlckd5660@gmail.com>
2022-07-11 16:41:55 +0900

    tdmpc 모델 정리

--- link_rl/c_encoders/a_encoder.py
+++ link_rl/c_encoders/a_encoder.py
@@ -13,6 +13,7 @@ class ENCODER(enum.Enum):
     IdentityEncoder = "IdentityEncoder"
     SimpleConvEncoder = "SimpleConvEncoder"
     NatureAtariEncoder = "NatureAtariEncoder"
+    SimpleTdmpcEncoder = "SimpleTdmpcEncoder"
 
 
 class BaseEncoder(ABC):
@@ -20,13 +21,14 @@ class BaseEncoder(ABC):
         self._observation_shape = observation_shape
         self._n_channels = observation_shape[0]
 
-        def get_encoder_out(conv_layers, shape) -> int:
-            conv_layers.eval()
-            encoder_out = conv_layers(torch.zeros(1, *shape))
-            return int(np.prod(encoder_out.size()))
         encoder = self._create_encoder()
-        self._encoder_out = get_encoder_out(encoder, self._observation_shape)
+        self._encoder_out = self.get_encoder_out(encoder, self._observation_shape)
         del encoder
+
+    def get_encoder_out(self, conv_layers, shape) -> int:
+        conv_layers.eval()
+        encoder_out = conv_layers(torch.zeros(1, *shape))
+        return int(np.prod(encoder_out.size()))
 
     @abstractmethod
     def _create_encoder(self) -> nn.Module:
