5b36209c28e306c92b59fa1a40750a6a36b9848c
Youn-Hee Han <yh21.han@gmail.com>
2022-07-12 20:26:27 +0900

    evovgym 테스트 코드

--- link_rl/b_environments/evolution_gym/evoution_gym_test.py
+++ link_rl/b_environments/evolution_gym/evoution_gym_test.py
@@ -3,17 +3,23 @@ import evogym.envs
 from evogym import sample_robot
 
 if __name__ == '__main__':
-	body, connections = sample_robot((5,5))
+	body, connections = sample_robot((9, 9))
 	env = gym.make('Walker-v0', body=body)
-	env.reset()
 
-	while True:
-		action = env.action_space.sample() - 1
-		ob, reward, done, info = env.step(action)
-		env.render()
+	for ep in range(1, 10):
+		observation = env.reset()
+		done = False
+		step = 1
 
-		if done:
-			env.reset()
+		while not done:
+			action = env.action_space.sample() - 1
+			next_observation, reward, done, info = env.step(action)
+			print(
+				"[EP: {0}, STEP: {1}] Observation: {2}, Action: {3}, next_observation: {4}, Reward: {5:.5f}, Done: {6}".format(
+					ep, step, observation.shape, action.shape, next_observation.shape, reward, done
+			))
+			env.render()
+			observation = next_observation
+			step += 1
 
 	env.close()
-	
