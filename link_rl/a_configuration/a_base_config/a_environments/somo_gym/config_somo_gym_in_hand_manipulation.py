dd69d63ce93f4c23182880cc80e0d6e8aef62c2e
Youn-Hee Han <yh21.han@gmail.com>
2022-07-26 00:03:09 +0900

    minor change

--- link_rl/a_configuration/a_base_config/a_environments/somo_gym/config_somo_gym_in_hand_manipulation.py
+++ link_rl/a_configuration/a_base_config/a_environments/somo_gym/config_somo_gym_in_hand_manipulation.py
@@ -4,5 +4,5 @@ from link_rl.a_configuration.a_base_config.a_environments.somo_gym import Config
 class ConfigSomoGymInHandManipulation(ConfigSomoGym):
     def __init__(self):
         self.ENV_NAME = "InHandManipulation"
-        self.EPISODE_REWARD_MIN_SOLVED = 1_000
+        self.EPISODE_REWARD_MIN_SOLVED = 2_000
         super(ConfigSomoGymInHandManipulation, self).__init__(self.ENV_NAME)
