86c8008861ce5b6fca21f4c727a2fbfe1ca72e90
zzzanghun <wlckd5660@gmail.com>
2022-07-25 16:26:10 +0900

    add tdmpc policy parameterized

--- link_rl/d_models/h_tdmpc_model.py
+++ link_rl/d_models/h_tdmpc_model.py
@@ -11,7 +11,8 @@ from collections import OrderedDict
 
 class TDMPC_MODEL(enum.Enum):
     TdmpcModel = "TdmpcModel"
-    TdmpcModelParameterizedPlanAction = "TdmpcModelParameterizedPlanAction"
+    TdmpcModelEachParameterizedPolicyAction = "TdmpcModelEachParameterizedPolicyAction"
+    TdmpcModelParameterizedPolicyAction = "TdmpcModelParameterizedPolicyAction"
 
 
 class _TruncatedNormal(pyd.Normal):
@@ -171,7 +172,7 @@ class TdmpcModel(SingleModel):
 
 
 @model_registry.add
-class TdmpcModelParameterizedPlanAction(SingleModel):
+class TdmpcModelEachParameterizedPolicyAction(SingleModel):
     class TOLDModel(nn.Module):
         def __init__(
                 self, representation_net, dynamics_net, reward_net, pi_net, q1_net, q2_net, policy_parameterized_net,
@@ -291,9 +292,133 @@ class TdmpcModelParameterizedPlanAction(SingleModel):
             policy_parameterized_net_dict["policy_parameterized_{0}".format(i)] = nn.Linear(512, 1)
         policy_parameterized_net = nn.Sequential(policy_parameterized_net_dict)
 
-        told_model = TdmpcModelParameterizedPlanAction.TOLDModel(
+        told_model = TdmpcModelEachParameterizedPolicyAction.TOLDModel(
             representation_net, dynamics_net, reward_net, pi_net, q1_net, q2_net, policy_parameterized_net,
             self._n_out_actions
+        )
+
+        return told_model
+
+
+@model_registry.add
+class TdmpcModelParameterizedPolicyAction(SingleModel):
+    class TOLDModel(nn.Module):
+        def __init__(
+                self, representation_net, dynamics_net, reward_net, pi_net, q1_net, q2_net,
+        originar_parameterized_net, scale_parameterized_net, bias_parameterized_net):
+            super().__init__()
+            self.representation_net = representation_net
+            self.dynamics_net = dynamics_net
+            self.reward_net = reward_net
+            self.pi_net = pi_net
+            self.q1_net = q1_net
+            self.q2_net = q2_net
+            self.originar_parameterized_net = originar_parameterized_net
+            self.scale_parameterized_net = scale_parameterized_net
+            self.bias_parameterized_net = bias_parameterized_net
+
+            self.apply(_orthogonal_init)
+
+            for m in [self.reward_net, self.q1_net, self.q2_net]:
+                m[-1].weight.data.fill_(0)
+                m[-1].bias.data.fill_(0)
+
+        def forward(self, obs):
+            z = self.representation(obs)
+            a = self.pi_net(z)
+            q1, q2 = self.Q(z, a)
+            dynamics, reward = self.next(z, a)
+            return q1, q2, dynamics, reward
+
+        def track_q_grad(self, enable=True):
+            """Utility function. Enables/disables gradient tracking of Q-networks."""
+            for m in [self.q1_net, self.q2_net]:
+                _set_requires_grad(m, enable)
+
+        def representation(self, obs):
+            """Encodes an observation into its latent representation (h)."""
+            return self.representation_net(obs)
+
+        def next(self, z, a):
+            """Predicts next latent state (d) and single-step reward (R)."""
+            x = torch.cat([z, a], dim=-1)
+            return self.dynamics_net(x), self.reward_net(x)
+
+        def pi(self, z, std=0):
+            """Samples an action from the learned policy (pi)."""
+            z = self.pi_net(z)
+            a = self.originar_parameterized_net(z)
+            k = self.scale_parameterized_net(z)
+            a0 = self.bias_parameterized_net(z)
+            mu = torch.tanh(k*a - k*a0)
+            if std > 0:
+                std = torch.ones_like(mu) * std
+                return _TruncatedNormal(mu, std).sample(clip=0.3)
+            return mu
+
+        def Q(self, z, a):
+            """Predict state-action value (Q)."""
+            x = torch.cat([z, a], dim=-1)
+            return self.q1_net(x), self.q2_net(x)
+
+    def __init__(
+        self,
+        n_input: int,
+        n_out_actions: int,
+        n_discrete_actions=None
+    ):
+        super().__init__(
+            n_input,
+            n_out_actions,
+            n_discrete_actions
+        )
+
+    @final
+    def _create_model(self) -> Tuple[nn.Module, nn.Module]:
+        representation_net = nn.Sequential(
+            nn.Linear(self._n_input, 256),
+            nn.ELU(),
+            nn.Linear(256, 50)
+        )
+
+        dynamics_net = nn.Sequential(
+            nn.Linear(50 + self._n_out_actions, 512),
+            nn.ELU(),
+            nn.Linear(512, 512),
+            nn.ELU(),
+            nn.Linear(512, 50))
+
+        reward_net = nn.Sequential(
+            nn.Linear(50 + self._n_out_actions, 512),
+            nn.ELU(),
+            nn.Linear(512, 512),
+            nn.ELU(),
+            nn.Linear(512, 1))
+
+        pi_net = nn.Sequential(
+            nn.Linear(50, 512),
+            nn.ELU(),
+            nn.Linear(512, 512),
+            nn.ELU())
+
+        q1_net = nn.Sequential(nn.Linear(50 + self._n_out_actions, 512),
+                               nn.LayerNorm(512),
+                               nn.Tanh(),
+                               nn.Linear(512, 512), nn.ELU(),
+                               nn.Linear(512, 1))
+        q2_net = nn.Sequential(nn.Linear(50 + self._n_out_actions, 512),
+                               nn.LayerNorm(512),
+                               nn.Tanh(),
+                               nn.Linear(512, 512), nn.ELU(),
+                               nn.Linear(512, 1))
+
+        original_parameterized_net = nn.Linear(512, self._n_out_actions)
+        scale_parameterized_net = nn.Linear(512, self._n_out_actions)
+        bias_parameterized_net = nn.Linear(512, self._n_out_actions)
+
+        told_model = TdmpcModelParameterizedPolicyAction.TOLDModel(
+            representation_net, dynamics_net, reward_net, pi_net, q1_net, q2_net,
+            original_parameterized_net, scale_parameterized_net, bias_parameterized_net
         )
 
         return told_model
