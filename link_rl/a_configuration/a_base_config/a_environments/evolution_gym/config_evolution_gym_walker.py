e48ad77b7fa605a8b40c1487c7241914d8681b7b
zzzanghun <wlckd5660@gmail.com>
2022-07-25 13:20:51 +0900

    change evogym walker robot

--- link_rl/a_configuration/a_base_config/a_environments/evolution_gym/config_evolution_gym_walker.py
+++ link_rl/a_configuration/a_base_config/a_environments/evolution_gym/config_evolution_gym_walker.py
@@ -1,11 +1,22 @@
 from link_rl.a_configuration.a_base_config.a_environments.evolution_gym import ConfigEvolutionGym
-
+import numpy as np
 
 class ConfigEvolutionGymWalker(ConfigEvolutionGym):
     def __init__(self):
         super(ConfigEvolutionGymWalker, self).__init__()
         self.ENV_NAME = "Walker-v0"
         self.EPISODE_REWARD_MIN_SOLVED = 1_000
+
+        self.ROBOT_SHAPE = (5, 5)
+        # self.ROBOT_STRUCTURE, self.ROBOT_CONNECTIONS = sample_robot(self.ROBOT_SHAPE)
+
+        self.ROBOT_STRUCTURE = np.asarray([
+            [3, 3, 3, 3, 3],
+            [3, 3, 3, 3, 3],
+            [3, 3, 0, 3, 3],
+            [3, 3, 0, 3, 3],
+            [3, 3, 0, 3, 3],
+        ])
 
 
 class ConfigEvolutionGymBridgeWalker(ConfigEvolutionGym):
