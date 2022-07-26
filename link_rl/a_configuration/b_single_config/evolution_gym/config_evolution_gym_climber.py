ea40070e6b56de34dff313974d154145962659d5
zzzanghun <wlckd5660@gmail.com>
2022-07-26 21:21:38 +0900

    somo gym, evo gym ppo config 추가

--- link_rl/a_configuration/b_single_config/evolution_gym/config_evolution_gym_climber.py
+++ link_rl/a_configuration/b_single_config/evolution_gym/config_evolution_gym_climber.py
@@ -1,7 +1,9 @@
 from link_rl.a_configuration.a_base_config.a_environments.evolution_gym.config_evolution_gym_climber import \
     ConfigEvolutionGymClimberV0, ConfigEvolutionGymClimberV1, ConfigEvolutionGymClimberV2
 from link_rl.a_configuration.a_base_config.b_agents.config_agents_off_policy import ConfigSac, ConfigTdmpc
+from link_rl.a_configuration.a_base_config.b_agents.config_agents_on_policy import ConfigPpo
 from link_rl.a_configuration.a_base_config.config_single_base import ConfigBase
+from link_rl.d_models.d_basic_actor_critic_model import BASIC_ACTOR_CRITIC_MODEL
 from link_rl.d_models.g_sac_model import SAC_MODEL
 from link_rl.d_models.h_tdmpc_model import TDMPC_MODEL
 
@@ -43,6 +45,39 @@ class ConfigEvolutionGymClimberV2Sac(ConfigBase, ConfigEvolutionGymClimberV2, Co
         self.ALPHA_LEARNING_RATE = 0.000025
         self.MIN_ALPHA = 0.2
         self.MODEL_TYPE = SAC_MODEL.ContinuousSacModel.value
+
+
+class ConfigEvolutionGymClimberV0Ppo(ConfigBase, ConfigEvolutionGymClimberV0, ConfigPpo):
+    def __init__(self):
+        ConfigBase.__init__(self)
+        ConfigEvolutionGymClimberV0.__init__(self)
+        ConfigPpo.__init__(self)
+
+        self.MAX_TRAINING_STEPS = 1_000_000
+        self.BUFFER_CAPACITY = 500_000
+        self.MODEL_TYPE = BASIC_ACTOR_CRITIC_MODEL.ContinuousBasicActorCriticSharedModel.value
+
+
+class ConfigEvolutionGymClimberV1Ppo(ConfigBase, ConfigEvolutionGymClimberV1, ConfigPpo):
+    def __init__(self):
+        ConfigBase.__init__(self)
+        ConfigEvolutionGymClimberV1.__init__(self)
+        ConfigPpo.__init__(self)
+
+        self.MAX_TRAINING_STEPS = 1_000_000
+        self.BUFFER_CAPACITY = 500_000
+        self.MODEL_TYPE = BASIC_ACTOR_CRITIC_MODEL.ContinuousBasicActorCriticSharedModel.value
+
+
+class ConfigEvolutionGymClimberV2Ppo(ConfigBase, ConfigEvolutionGymClimberV2, ConfigPpo):
+    def __init__(self):
+        ConfigBase.__init__(self)
+        ConfigEvolutionGymClimberV2.__init__(self)
+        ConfigPpo.__init__(self)
+
+        self.MAX_TRAINING_STEPS = 1_000_000
+        self.BUFFER_CAPACITY = 500_000
+        self.MODEL_TYPE = BASIC_ACTOR_CRITIC_MODEL.ContinuousBasicActorCriticSharedModel.value
 
 
 class ConfigEvolutionGymClimberV0Tdmpc(ConfigBase, ConfigEvolutionGymClimberV0, ConfigTdmpc):
